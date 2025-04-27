bc947fbb76816f489980c5a16416e000f3553dac
sathish-1023 <106345094+sathish-1023@users.noreply.github.com>
2025-04-27 20:56:19 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         </div>
       
       </div>
-      <ComputersCanvas/>
+      
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary 
